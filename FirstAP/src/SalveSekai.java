0c56c3eeba81a4d8ee5e66495f517c7e8e9fef1d
Jenny <jenny.tan0109@gmail.com>
2017-02-26 16:15:39 -0500

    This is my first check-in

---
+++ FirstAP/src/SalveSekai.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jenny on 26/02/2017.
+ */
+public class SalveSekai {
+    public static void main(String[] args)
+    {
+        System.out.println("I said, \"she is a girl.\"");
+        System.out.println("This is another line");
+    }
+}
